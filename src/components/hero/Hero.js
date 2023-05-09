444be0fd471043345d081ef1473852101cc66b7c
Aatif Ali <aatifwarraich10@gmail.com>
2023-05-09 21:58:31 +0500

    hero section

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="conatiner">
+        <div className="left-side"></div>
+        <div className="right-side"></div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
