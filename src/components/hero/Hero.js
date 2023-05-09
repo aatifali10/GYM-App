bbb140e70b1f05ae55d94dd280c2a3a48669e44b
Aatif Ali <aatifwarraich10@gmail.com>
2023-05-09 23:17:26 +0500

    header

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,12 +1,29 @@
 import React from "react";
+import Header from "../header/Header";
 import "./hero.css";
 
 const Hero = () => {
   return (
     <>
-      <div className="conatiner">
-        <div className="left-side"></div>
-        <div className="right-side"></div>
+      <div className="hero">
+        <div className="left-side">
+          <Header />
+          <div className="the-ad">
+            <div></div>
+            <span>the best fitness club in the town</span>
+          </div>
+          {/* Hero text  */}
+          <div className="hero-text">
+            <div>
+              <span>Shape </span>
+              <span>Your</span>
+            </div>
+            <div>
+              <span>Ideal body</span>
+            </div>
+          </div>
+        </div>
+        <div className="right-side">right side</div>
       </div>
     </>
   );
