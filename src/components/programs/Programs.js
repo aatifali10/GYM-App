31b5f1d898479daa579a9c19782cf9082a64f2f3
Aatif Ali <aatifwarraich10@gmail.com>
2023-05-12 15:29:21 +0500

    programs card

--- src/components/programs/Programs.js
+++ src/components/programs/Programs.js
@@ -1,43 +1,30 @@
 import React from "react";
 import "./programs.css";
+import { programsData } from "../../data/programsData";
+import RightArrow from "../../assets/rightArrow.png";
 
 const Programs = () => {
-  const cards = [
-    {
-      image: "../../assets/",
-      title: "Strength Training",
-      para: "In this ",
-      button: "Join Now",
-      icon: "",
-    },
-    {
-      image: "",
-      title: "Strength Training",
-      para: "In this ",
-      button: "Join Now",
-      icon: "",
-    },
-    {
-      image: "",
-      title: "Strength Training",
-      para: "In this ",
-      button: "Join Now",
-      icon: "",
-    },
-    {
-      image: "",
-      title: "Strength Training",
-      para: "In this ",
-      button: "Join Now",
-      icon: "",
-    },
-  ];
   return (
     <>
-      <div className="programs">
-        <span className="stroke-text">Explore our</span>
-        <span>Programs</span>
-        <span className="stroke-text">To Share you</span>
+      <div className="Programs" id="programs">
+        <div className="programs-header">
+          <span className="stroke-text">Explore our</span>
+          <span>Programs</span>
+          <span className="stroke-text">To Share you</span>
+        </div>
+        <div className="program-categories">
+          {programsData.map((program) => (
+            <div className="category">
+              {program.image}
+              <span>{program.heading}</span>
+              <span>{program.details}</span>
+              <div className="join-now">
+                <span>Join Nows</span>
+                <img src={RightArrow} alt="" />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
