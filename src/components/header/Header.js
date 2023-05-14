3497297df094e6466bd483f0651aa4772a150b42
Aatif Ali <aatifwarraich10@gmail.com>
2023-05-14 21:57:12 +0500

    responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import logo from "../../assets/logo.png";
+import Bars from "../../assets/bars.png";
+// import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
 
 const Header = () => {
+  const mobile = window.innerHeight <= 768 ? true : false;
+  const [menuOpened, setMenuOpened] = useState(false);
   return (
     <>
       <div className="header">
         <img src={logo} alt="logo" className="logo" />
-        <ul className="header-menu">
-          <li>Home</li>
-          <li>Programs</li>
-          <li>Why us</li>
-          <li>Plans</li>
-          <li>Testimonials</li>
-        </ul>
+        {menuOpened === false && mobile === true ? (
+          <div
+            style={{
+              backgroundColor: "var(--appColor)",
+              padding: "0.5rem",
+              borderRadius: "5px",
+            }}
+            onClick={() => setMenuOpened(true)}
+          >
+            <img
+              src={Bars}
+              alt=""
+              style={{ width: "1.5rem", height: "1.5rem" }}
+            />
+          </div>
+        ) : (
+          <ul className="header-menu">
+            <li onClick={() => setMenuOpened(false)}>
+              <Link to="home" span={true} smooth={true} activeClass="active">
+                Home
+              </Link>
+            </li>
+            <li onClick={() => setMenuOpened(false)}>
+              <Link to="programs" span={true} smooth={true}>
+                Programs
+              </Link>
+            </li>
+            <li onClick={() => setMenuOpened(false)}>
+              <Link to="why-us" span={true} smooth={true}>
+                Why us
+              </Link>
+            </li>
+            <li onClick={() => setMenuOpened(false)}>
+              <Link to="plans" span={true} smooth={true}>
+                Plans
+              </Link>
+            </li>
+
+            <li onClick={() => setMenuOpened(false)}>
+              <Link to="testimonials" span={true} smooth={true}>
+                Testimonials
+              </Link>
+            </li>
+          </ul>
+        )}
       </div>
     </>
   );
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -6,18 +6,20 @@ import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
 import { motion } from "framer-motion";
+import NumberCounter from "number-counter";
 
 const Hero = () => {
   const transition = { type: "spring", duration: 3 };
+  const mobile = window.innerHeight <= 768 ? true : false;
   return (
     <>
-      <div className="hero">
+      <div className="hero" id="home">
         <div className="blur blur-h"></div>
         <div className="left-side">
           <Header />
           <div className="the-ad">
             <motion.div
-              initial={{ left: "238px" }}
+              initial={{ left: mobile ? "165px" : "238px" }}
               whileInView={{ left: "8px" }}
               transition={{ ...transition, type: "tween" }}
             ></motion.div>
@@ -43,15 +45,23 @@ const Hero = () => {
           {/* figures */}
           <div className="figures">
             <div>
-              <span>+140</span>
+              <span>
+                <NumberCounter end={140} start={100} delay="4" prefix="+" />
+              </span>
               <span>Expert coachs</span>
             </div>
             <div>
-              <span>+978</span>
+              <span>
+                {" "}
+                <NumberCounter end={978} start={800} delay="4" prefix="+" />
+              </span>
               <span>Member joind</span>
             </div>
             <div>
-              <span>+50</span>
+              <span>
+                {" "}
+                <NumberCounter end={50} start={0} delay="4" prefix="+" />
+              </span>
               <span>Fitness programs</span>
             </div>
           </div>
